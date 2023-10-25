8309916c2cbf0230a1ef6d73baaf290a8dd09ed4
Omar Facundo <114097196+omar5o9@users.noreply.github.com>
2023-10-25 16:51:51 +0000

    chalk and figlet

---
+++ index.js
@@ -0,0 +1,11 @@
+import figlet from "figlet";
+
+
+figlet("Omar", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
